7a4880b0bf69a732816bb397e831f8f6799a54cb
Serhii Holub <serhii.holub@valtech.com>
2022-07-22 12:06:30 +0300

    Enum type

--- array-type.ts
+++ array-type.ts
@@ -7,18 +7,11 @@ const person: {
     name: string;
     age: number;
     hobbies: string[];
-    role: [number, string]
 } = {
     name: 'Serhii',
     age: 48,
     hobbies: ['Sports', 'Cooking'],
-    role: [1, 'author']
 };
-
-person.role.push('admin');
-// person.role[1] = 10; // !!! ERROR
-// person.role = ['admin', 10]; // !!! ERROR
-// person.role = [10, 'admin', 10]; // !!! ERROR
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
